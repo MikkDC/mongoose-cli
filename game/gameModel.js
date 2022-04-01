be72c8d4faa531be9e635085ca5bca3f5f20a8b0
Michael C <mikkdc@yahoo.co.uk>
2022-04-01 21:03:46 +0100

    moved Model to its own modular file

---
+++ game/gameModel.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose")
+
+const Game = mongoose.model("Game", {
+    title: {type: String, unique: true, required: true },
+    developer: {type: String, unique: false, default: "Unknown" },
+    publisher: {type: String, unique: false, default: "Unknown" },
+    year: {type: Number, default: "????" }
+ });
+
+ module.exports = Game
